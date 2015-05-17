13cb46470a22975432b6b3a7f16e5612fa9a8b66
Pierre PIRONIN <pierre@pierrepironin.fr>
2015-05-17 22:32:27 +0200

    Validate computation form.

--- public/modules/dju/controllers/dju.client.controller.js
+++ public/modules/dju/controllers/dju.client.controller.js
@@ -27,7 +27,22 @@ angular.module('dju').controller('DjuController', ['DjuFile', 'Dju', 'ngTablePar
         self.recordsTable = new ngTableParams();
 
         // Computation
-        self.weekDays = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+        self.weekDays = [
+            {id:0, value:'Lundi'},
+            {id:1, value:'Mardi'},
+            {id:2, value:'Mercredi'},
+            {id:3, value:'Jeudi'},
+            {id:4, value:'Vendredi'},
+            {id:5, value:'Samedi'},
+            {id:6, value:'Dimanche'}
+        ];
+        self.regexDay = '(0?[1-9]|[1-2][0-9]|3[0-1])';
+        self.regexMonth = '(0?[1-9]|1[0-2])';
+        self.regexHours = '([01]?[0-9]|2[0-3])';
+        self.regexMinutes = '([0-5][0-9])';
+        self.patternDate = '/^' + self.regexDay + '\/' + self.regexMonth + '$/';
+        self.patternTimestamp = '/^' + self.regexHours + '(:' + self.regexMinutes + ')?$/';
+
         self.computation = {
             startDate: null,
             endDate: null,
@@ -36,12 +51,12 @@ angular.module('dju').controller('DjuController', ['DjuFile', 'Dju', 'ngTablePar
             endHour: null
         }
         self.toggleDay = function(day) {
-            var index = self.computation.weekDays.indexOf(day);
+            var index = self.computation.weekDays.indexOf(day.id);
 
             if( index > -1) {
                 self.computation.weekDays.splice(index, 1);
             } else {
-                self.computation.weekDays.push(day);
+                self.computation.weekDays.push(day.id);
             }
         }
 
