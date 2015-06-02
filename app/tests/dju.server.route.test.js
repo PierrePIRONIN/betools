00fc4f4a80cfa5fcd089cc21e0cb9fdd167b37d9
Pierre PIRONIN <pierre@pierrepironin.fr>
2015-06-02 12:18:55 +0200

    Revert from computing an array.

--- app/tests/dju.server.route.test.js
+++ app/tests/dju.server.route.test.js
@@ -22,18 +22,17 @@ describe('dju computation', function () {
     it('from 01/10 to 31/04, all the week, with 20°C all the day should return 2661', function (done) {
         request(app)
             .post('/computeDju')
-            .send([{
-                label: 'heating',
+            .send({
                 temperature: 20,
                 startDate: '01/10',
                 endDate: '31/04',
                 weekDays: [0, 1, 2, 3, 4, 5, 6],
                 startHour: '00:00',
                 endHour: '23:59'
-            }])
+            })
             .end(function (err, response) {
                 response.status.should.be.equal(200);
-                response.body.dju.should.be.exactly('2772');
+                response.body.dju.should.be.equal('2772');
                 done();
             });
     });
@@ -41,18 +40,17 @@ describe('dju computation', function () {
     it('from 15/10 to 15/04, all the week, with 20°C from 8:00 to 18:00 should return 959', function (done) {
         request(app)
             .post('/computeDju')
-            .send([{
-                label: 'heating',
+            .send({
                 temperature: 20,
                 startDate: '15/10',
                 endDate: '15/04',
                 weekDays: [0, 1, 2, 3, 4, 5, 6],
                 startHour: '08:00',
                 endHour: '18:00'
-            }])
+            })
             .end(function (err, response) {
                 response.status.should.be.equal(200);
-                response.body.dju.should.be.exactly('959');
+                response.body.dju.should.be.equal('959');
                 done();
             });
     });
@@ -60,18 +58,17 @@ describe('dju computation', function () {
     it('from 15/10 to 15/04, all the week, with 18°C from 8:00 to 18:00 should return 807', function (done) {
         request(app)
             .post('/computeDju')
-            .send([{
-                label: 'heating',
+            .send({
                 temperature: 18,
                 startDate: '15/10',
                 endDate: '15/04',
                 weekDays: [0, 1, 2, 3, 4, 5, 6],
                 startHour: '08:00',
                 endHour: '18:00'
-            }])
+            })
             .end(function (err, response) {
                 response.status.should.be.equal(200);
-                response.body.dju.should.be.exactly('807');
+                response.body.dju.should.be.equal('807');
                 done();
             });
     });
@@ -79,18 +76,17 @@ describe('dju computation', function () {
     it('from 15/10 to 15/04, no week-end, with 20°C from 8:00 to 17:00 should return 627', function (done) {
         request(app)
             .post('/computeDju')
-            .send([{
-                label: 'heating',
+            .send({
                 temperature: 20,
                 startDate: '15/10',
                 endDate: '15/04',
                 weekDays: [0, 1, 2, 3, 4],
                 startHour: '08:00',
                 endHour: '17:00'
-            }])
+            })
             .end(function (err, response) {
                 response.status.should.be.equal(200);
-                response.body.dju.should.be.exactly('627');
+                response.body.dju.should.be.equal('627');
                 done();
             });
     });
