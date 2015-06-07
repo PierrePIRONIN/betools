0706af7324929bf2fc30e64dbb1d5c3fb96449fc
Pierre PIRONIN <pierre@pierrepironin.fr>
2015-06-07 21:31:51 +0200

    First grunt build

--- config/env/production.js
+++ config/env/production.js
@@ -7,9 +7,11 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.min.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+				'public/lib/ng-table/dist/ng-table.min.css'
 			],
 			js: [
 				'public/lib/angular/angular.min.js',
+				'public/lib/angular-i18n/angular-locale_fr-fr.min.js',
 				'public/lib/angular-resource/angular-resource.js', 
 				'public/lib/angular-cookies/angular-cookies.js', 
 				'public/lib/angular-animate/angular-animate.js', 
@@ -17,7 +19,8 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+				'public/lib/ng-table/dist/ng-table.min.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
